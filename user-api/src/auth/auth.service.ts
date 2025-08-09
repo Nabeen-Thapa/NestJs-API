a113ff99854fb5962b7e0f4ae02d30de2c630524
Nabeen-Thapa <nabeen2058@gmail.com>
2025-08-09 15:49:20 +0545

    Add signup functionality completed

--- user-api/src/auth/auth.service.ts
+++ user-api/src/auth/auth.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { Prisma as PrismaNamespace } from '@prisma/client';
 
 
 @Injectable({})
-export class AuthService{
-    constructor(private prisma: PrismaService){}
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
 
-    async signup(dto:AuthDto) {
+    async signup(dto: AuthDto) {
         //generate password hash
-        const hash =await argon.hash(dto.password as string)
-        const user = await this.prisma.user.create({
-            data:{
-                email: dto.email as string,
-                hash
-            }
-        })
-        return user;
+        try {
+            const hash = await argon.hash(dto.password as string)
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email as string,
+                    hash
+                }
+            })
+
+            return user;
+        } catch (error) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      
+      throw error;
+        }
+
     }
 
-  
+
     signin() {
         return 'i am sign in';
     }
