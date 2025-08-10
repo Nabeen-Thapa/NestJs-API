6eb7678b9528f9bbe68547f544774d926537486b
Nabeen-Thapa <nabeen2058@gmail.com>
2025-08-10 18:54:55 +0545

    Add jwt validation

--- user-api/src/users/users.controller.ts
+++ user-api/src/users/users.controller.ts
@@ -1,19 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get("me")
+    getMe(@Request() req) {
+        console.log("controller:",req.user)
+        return req.user;
+    }
     @Get()
-    getALlUsers(){
+    getALlUsers() {
         return [];
     }
 
     @Get(":id")
-    getSpecificUser(@Param("id")id:string ){
+    getSpecificUser(@Param("id") id: string) {
         return id;
     }
 
     @Post()
-    createUser(@Body() user:{}){
-        
+    createUser(@Body() user: {}) {
+
     }
 }
