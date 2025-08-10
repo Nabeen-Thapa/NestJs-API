f3f4da532ee0e9f5310f9ef0d56023d50aeef892
Nabeen-Thapa <nabeen2058@gmail.com>
2025-08-10 17:33:10 +0545

    Add access token generated

--- user-api/src/auth/auth.service.ts
+++ user-api/src/auth/auth.service.ts
@@ -2,12 +2,12 @@ import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
-import { Prisma as PrismaNamespace } from '@prisma/client';
-
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config:ConfigService) { }
 
     async signup(dto: AuthDto) {
         //generate password hash
@@ -42,9 +42,22 @@ export class AuthService {
 
         const pwdMatches = await argon.verify(user.hash, dto.password as string)
         if (!pwdMatches) throw new ForbiddenException("invalid passoword");
-        
-        const { hash, ...userWithoutHash } = user;
-        return userWithoutHash;
 
+        // const { hash, ...userWithoutHash } = user;
+        // return userWithoutHash;
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: number, email:string):Promise<{access_token: string}>{
+        const payload = {
+            sub: userId,
+            email
+        }
+        const secret = this.config.get("JWT_SECRET");
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: "15m",
+            secret: secret,
+        });
+        return {access_token: token};
     }
 }
